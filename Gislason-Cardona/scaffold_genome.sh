e4dadb97bef90967b1293f6c339e3aec676018e8
Keith H. Turner <khturner@gmail.com>
2017-01-23 19:35:35 -0600

    Update scaffold_genome.sh

--- Gislason-Cardona/scaffold_genome.sh
+++ Gislason-Cardona/scaffold_genome.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 # This is a work in progress, don't run as a shell script!
 
+# get the docker image, hop into it
 docker pull khturner/tn-seq
 docker run -it khturner/tn-seq bash
+
+# Start installing some dependencies
+cd
+apt-get install python-setuptools python-dev build-essential
+easy_install pip
+pip install -U FastaIndex
+
+# Install the LAST dependency
+wget http://last.cbrc.jp/last-830.zip
+unzip last-830.zip
+cd last-830
+make
+make install
+cd
+
+# Move into a working directory
+mkdir scaffolding
+cd scaffolding
+git clone https://github.com/lpryszcz/pyScaf.git
+
+# Get the K56 genome
+aws s3 configure # put in aws access keys and the region is us-east-1
+aws s3 cp s3://cenocepacia-tnseq/k56.fasta ./
+
+# Next up, download the J2315 genome and try scaffolding it
 
