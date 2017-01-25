ed307ecee3e032f24e46b9287829bb1e92471d36
Keith H. Turner <khturner@gmail.com>
2017-01-25 03:36:46 +0000

    changed process_reads.py

--- py/process_reads.py
+++ py/process_reads.py
@@ -8,8 +8,8 @@ parser.add_argument('-r', '--reference', help = 'Reference genome to map against
 parser.add_argument('-p', '--primer', help = 'Sequence of the primer used to amplify your TnSeq library', required = True)
 parser.add_argument('-i', '--invertedrepeat', help = 'Sequence of the Tn end after your primer sequence but before the insertion site', required = True)
 parser.add_argument('-m', '--mismatches', help = 'Number of mismatches to allow when searching reads for the primer/repeat signature', default = 1)
-parser.add_argument('-1', '--read1', help = 'Read 1 file (FASTQ). This is the read that the Tn end is presumed to be on', required = True)
-parser.add_argument('-2', '--read2', help = 'Read 2 file (FASTQ) if paired end sequencing performed. This will be used in mapping')
+parser.add_argument('-1', '--read1', help = 'Read 1 file (FASTQ)', required = True)
+parser.add_argument('-2', '--read2', help = 'Read 2 file (FASTQ) if paired end sequencing performed and Tn end could be on either read')
 
 args = parser.parse_args()
 # print(args) # debug
