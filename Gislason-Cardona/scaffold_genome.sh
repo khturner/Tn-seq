3d9343d15d1facdecc802d3594e715ebfc3e572f
Keith H. Turner <khturner@gmail.com>
2017-01-24 19:02:48 -0600

    Scaffolding figured out
    Gets us to two ~3.5 Mbp scaffolds and some assorted contigs

--- Gislason-Cardona/scaffold_genome.sh
+++ Gislason-Cardona/scaffold_genome.sh
@@ -31,4 +31,4 @@ aws s3 cp s3://cenocepacia-tnseq/k56.fasta ./
 # Next up, download the J2315 genome and try scaffolding it - IN PROGRESS
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/009/485/GCF_000009485.1_ASM948v1/GCF_000009485.1_ASM948v1_genomic.fna.gz
 gunzip GCF_000009485.1_ASM948v1_genomic.fna.gz
-pyScaf/pyScaf.py -f k56.fasta -t 4 --dotplot scafdots.png -r GCF_000009485.1_ASM948v1_genomic.fna -o k56_scaffolded.fna
+pyScaf/pyScaf.py -f k56.fasta -t 4 --dotplot png -g 1000000 -r GCF_000009485.1_ASM948v1_genomic.fna -o k56_scaffolded.fna
