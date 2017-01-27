89d18f865bda63f75f12df1eb37055a43d31c7e7
Keith H. Turner <khturner@gmail.com>
2017-01-27 13:23:15 -0600

    loess smoothing plots

--- R/obligate_essentiality_analysis.R
+++ R/obligate_essentiality_analysis.R
@@ -10,14 +10,18 @@ reference_fasta <- args[1]
 reference_gff <- args[2]
 features_of_interest <- args[3]
 three_prime_trim <- as.integer(args[4]) / 100
-output_prefix <- args[5]
-counts_files <- args[6:length(args)]
+ignore_sites <- as.integer(args[5])
+min_reads_per_site <- as.integer(args[6])
+output_prefix <- args[7]
+counts_files <- args[8:length(args)]
 
 # DEBUG
 reference_fasta <- "k56.fasta"
 reference_gff <- "k56.gff"
 features_of_interest <- "gene"
 three_prime_trim <- 10 / 100
+ignore_sites <- 0
+min_reads_per_site <- 1
 output_prefix <- "testan"
 counts_files <- "test.sites.tsv"
 
@@ -25,12 +29,16 @@ counts_files <- "test.sites.tsv"
 counts_data <- data.frame()
 for (counts_file in counts_files) {
   if (!file.exists(counts_file)) { stop(paste(counts_file, "Not found!")) }
-  counts_data <- read_tsv(counts_file) %>% mutate(counts_file = counts_file) %>% rbind(counts_data, .) %>% tbl_df
+  counts_data <- read_tsv(counts_file) %>%
+    arrange(desc(num_reads)) %>% filter(row_number() > ignore_sites) %>% # Cut off i highest sites
+    filter(num_reads >= min_reads_per_site) %>% # at least m reads/site
+    mutate(counts_file = counts_file) %>% rbind(counts_data, .) %>% tbl_df
 }
 counts_data <- counts_data %>% arrange(counts_file, template, position)
 
 # LOESS smoothing
 smoothed <- counts_data %>% group_by(counts_file, template) %>%
+#  filter(n() > 1) %>% # Can't smooth a single number...
   do(pred_num_reads = predict(loess(num_reads ~ position, data = ., span = 1,
         control = loess.control(statistics = c("approximate"), trace.hat = c("approximate"))), .$position))
 counts_data$pred_num_reads <- lapply(smoothed$pred_num_reads, unlist) %>% unlist
@@ -38,13 +46,23 @@ counts_data <- counts_data %>% group_by(counts_file, template) %>%
   mutate(adjustment_ratio = pred_num_reads / median(pred_num_reads),
          smoothed_num_reads = num_reads / adjustment_ratio) %>% ungroup
 
+reference_genome <- read.fasta(reference_fasta)
+template_sizes <- data.frame(template = names(reference_genome),
+                             template_length = unlist(lapply(reference_genome, length)),
+                             stringsAsFactors = F) %>% tbl_df
+
 # Visualize smoothing results
 for (counts_file_entry in unique(counts_data$counts_file)) {
-  counts_data %>% group_by(template) %>% filter(max(position) >= 10000) %>% # No short contigs
-    # filter(counts_file == counts_file_entry) %>%
+  p <- template_sizes %>% filter(template_length >= 20000) %>% inner_join(counts_data) %>% # No short contigs
+    filter(counts_file == counts_file_entry) %>%
+    group_by(template) %>% sample_frac(0.05) %>% # Only draw 5% of points for decluttering
     ggplot(aes(position)) + theme_bw() + scale_x_continuous(labels = scales::comma) +
-    geom_point(aes(y = num_reads), color = "#e41a1c", alpha = 0.2) +
-    geom_point(aes(y = smoothed_num_reads), color = "#4daf4a", alpha = 0.2) + 
-    geom_line(aes(y = pred_num_reads), color = "#377eb8") +
-    facet_wrap(~template, scales = "free")
+    geom_point(aes(y = num_reads, color = "Raw"), alpha = 0.5) +
+    geom_point(aes(y = smoothed_num_reads, color = "Smoothed"), alpha = 0.5) + 
+    geom_segment(aes(xend = position, y = num_reads, yend = smoothed_num_reads), color = "grey") +
+    geom_line(aes(y = pred_num_reads, color = "LOESS Trend")) +
+    facet_wrap(~template, scales = "free") +
+    scale_color_brewer("", type = "qual", palette = "Dark2") + theme(legend.position = "bottom") +
+    ggtitle("LOESS smoothing positional bias", counts_file_entry) + ylab("Number of reads") + xlab("Position")
+  ggsave(paste0(counts_file_entry, ".loess.png"), p, scale = 2, height = 4, width = 6, units = "in")
 }
--- py/obligate_essentiality_analysis.py
+++ py/obligate_essentiality_analysis.py
@@ -8,6 +8,8 @@ parser.add_argument('-r', '--reference', help = 'Reference genome to map against
 parser.add_argument('-g', '--gff', help = 'GFF file describing genomic annotations', required = True)
 parser.add_argument('-f', '--features', help = 'Features of interest to consider in essentiality analysis (default: %(default)s)', default = 'gene')
 parser.add_argument('-3', '--threeprimetrim', help = 'Percent of the 3\' end of genes to ignore insertions in (default: %(default)s)', default = 10)
+parser.add_argument('-i', '--ignore', help = 'Number of highest-read sites to ignore (default: %(default)s)', default = 0)
+parser.add_argument('-m', '--minreads', help = 'Minimum number of reads required to consider a site (default: %(default)s)', default = 2)
 parser.add_argument('-o', '--output', help = 'Output file prefix', required = True)
 parser.add_argument('-c', '--control', help = 'Insertion site read count data TSV file (specify additional times for replicates)', nargs = '+', required = True)
 #parser.add_argument('-t', '--test', help = 'Test condition site count data file (specify additional times for replicates)', action = 'append', required = True)
@@ -15,7 +17,7 @@ parser.add_argument('-c', '--control', help = 'Insertion site read count data TS
 args = parser.parse_args()
 
 cmd = ['Rscript', '--vanilla', sys.path[0] + '/../R/obligate_essentiality_analysis.R', args.reference, args.gff, args.features,
-       str(args.threeprimetrim), args.output] + args.control
+       str(args.threeprimetrim), str(args.ignore), str(args.minreads), args.output] + args.control
 print(cmd)
 p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 result, err = p.communicate()
